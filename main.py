244e039781ce95f36caa2f452ecf6054dfb995cc
Satoshi Innami <innamisatoshi@gmail.com>
2017-12-04 12:13:28 +0900

    add peers

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import json
+from urllib.parse import urlparse
 
-from bottle import post, get, run, template, request
+from bottle import post, get, run, request
 
 from blockchain import Blockchain
+from server import Peer
 
 blockchain = Blockchain()
+
+peers = []
 
 
 @post('/mineBlock')
@@ -17,7 +21,20 @@ def mine_block():
 @get('/blocks')
 def get_blocks():
     json_blockchain = blockchain.to_json()
-    return template(json_blockchain)
+    return json_blockchain
+
+
+@post('/addPeer')
+def add_peer():
+    dic = json.load(request.body)
+    url = urlparse(dic["peer"])
+    peers.append(Peer(url.hostname, url.port))
+
+
+@get('/peers')
+def get_peers():
+    json_peers = json.dumps([str(p) for p in peers])
+    return json_peers
 
 
 run(host='localhost', port=8080)
